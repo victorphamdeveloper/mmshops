e95ddf89e165c043a927c771e495354be0a9e1e4
victor <victorpham93@gmail.com>
2013-07-13 23:10:26 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mmshops::Application.config.secret_token = '38ffc44ed37fbe2393c1573803382cb8ab83d3795de7822d38819aff68e1c770418d043ff8c540e568c650bf8d0df0f5db3d61a669f3077c9d7eec8a5f6b89f5'
