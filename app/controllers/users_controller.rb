92c584f079931b6346ba793ae1cb14ff9790851d
victor <victorpham93@gmail.com>
2013-07-17 12:29:51 +0800

    Added some layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,10 +26,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @user }
-    end
+    puts "jahbdhadgadbsajcfvsjabchsabjhbsfcdbsjahbdfjsbjdbshbcdjsvchsvchsdv"
+
+
+    #respond_to do |format|
+     # format.html # new.html.erb
+      #format.json { render json: @user }
+    #end
   end
 
   # GET /users/1/edit
@@ -40,17 +43,20 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(params[:user])
+    
+    role = params[:role]
+    if(role == 3){
+      @user = Buyer.create(params[:user]) 
+    }else{
+      @user = Seller.create(params[:user])
+    }
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    if @user.save
+      puts @user.class
+      render 'new'
+    
+    
+    
   end
 
   # PUT /users/1
