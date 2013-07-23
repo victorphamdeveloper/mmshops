d70aa8f3182762b006031f9c6ce6e0308c17b83b
VU Phuc Tho <vuphuctho@gmail.com>
2013-07-23 16:44:00 +0800

    change at category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -10,18 +10,17 @@
 #
 
 class Category < ActiveRecord::Base
-  belongs_to :parent_category, class_name: "Category"
-  has_many :sub_categories, class_name: "Category", foreign_key: :parent_id
-  has_many :products
-  #has_many :products, through: :sub_categories
+  has_ancestry 
 
-  attr_accessible :name, :parent_id
+  has_many :products
+
+  attr_accessible :name
 
   def get_products
   	products = self.products.all
 
   	products.to_a
-  	self.sub_categories.each do |f|
+  	self.children.each do |f|
   		products.push f.get_products
   	end
   	products
